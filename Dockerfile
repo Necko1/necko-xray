d4e373e70dfb132bdf3b79ba1170e0108b8012c5
necko <cherh585@gmail.com>
2025-11-24 00:21:01 +0100

    Initial commit (with fillings)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,36 @@
-FROM ubuntu:latest
+FROM rust:alpine3.21 AS builder
+WORKDIR /app
+
+RUN apk add --no-cache musl-dev
+
+RUN mkdir src && echo "fn main() {}" > src/main.rs
+COPY Cargo.toml .
+RUN cargo build --release
+
+COPY src ./src
+RUN touch src/main.rs && cargo build --release
+
+
+FROM alpine:3.22
 LABEL authors="necko"
 
-ENTRYPOINT ["top", "-b"]
+# https://github.com/XTLS/Xray-core/releases
+ARG XRAY_VERSION=v25.10.15
+
+RUN apk add --no-cache curl unzip ca-certificates netcat-openbsd && \
+    mkdir -p /var/log/xray /usr/local/share/xray
+
+RUN curl -L -H "Cache-Control: no-cache" -o xray.zip https://github.com/XTLS/Xray-core/releases/download/${XRAY_VERSION}/Xray-linux-64.zip && \
+    unzip xray.zip && \
+    mv xray /usr/local/bin/xray && \
+    mv geoip.dat /usr/local/share/xray/ && \
+    mv geosite.dat /usr/local/share/xray/ && \
+    chmod +x /usr/local/bin/xray && \
+    rm xray.zip && \
+    apk del curl unzip
+
+ENV XRAY_LOCATION_ASSET=/usr/local/share/xray/
+
+COPY --from=builder /app/target/release/necko-xray /usr/local/bin/necko-xray
+
+CMD ["/usr/local/bin/necko-xray"]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Stdio;
+use tokio::process::Command;
+use tokio::signal::unix::{signal, SignalKind};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("[necko-xray]: Starting Xray...");
+
+    let mut child = Command::new("/usr/local/bin/xray")
+        .arg("-config")
+        .arg("/etc/xray/config.json")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .kill_on_drop(true)
+        .spawn()
+        .expect("Failed to start Xray process");
+
+    let pid = child.id().unwrap();
+    println!("[necko-xray]: Xray started with PID: {}", pid);
+
+    let mut sigterm = signal(SignalKind::terminate())?;
+    let mut sigint = signal(SignalKind::interrupt())?;
+
+    tokio::select! {
+        status = child.wait() => {
+            match status {
+                Ok(s) => println!("[necko-xray]: Xray exited unexpectedly with {}", s),
+                Err(e) => println!("[necko-xray]: Error waiting for Xray: {}", e),
+            }
+        }
+
+        _ = sigterm.recv() => {
+            println!("[necko-xray]: Received SIGTERM. Shutting down...");
+        }
+
+        _ = sigint.recv() => {
+            println!("[necko-xray]: Received SIGINT (Ctrl+C). Shutting down...");
+        }
+    }
+
+    Ok(())
 }
